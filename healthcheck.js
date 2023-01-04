505613fece320317ab1e6208f6aadd1056ab5fc3
Akash Kumar <20ee01071@iitbbs.ac.in>
2023-01-04 17:59:55 +0530

    commit

--- healthcheck.js
+++ healthcheck.js
@@ -1,6 +1,6 @@
 async function makeRequest() {
   try {
-    const response = await fetch('http://localhost:3000/notes/health');
+    const response = await fetch('http://localhost:3000/health');
 
     console.log('status code: ', response.status); 
 
